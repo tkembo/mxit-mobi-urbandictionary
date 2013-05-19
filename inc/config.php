21aff35911ca59f5b183761de84f2ddc56701d3f
Tawanda Kembo <tkembo@gmail.com>
2013-05-19 11:04:24 +0200

    forgot to change database settings
    what was I thinking?

--- inc/config.php
+++ inc/config.php
@@ -11,16 +11,17 @@
 	$GA_PIXEL = "ga.php";
 	
 	//DB Settings
+	/*
 	$hostname_mxit_urbandictionary = "localhost";
 	$database_mxit_urbandictionary = "mxiturbandictionary";
 	$username_mxit_urbandictionary = "root";
 	$password_mxit_urbandictionarys = "";
-	/*
+	*/
 	$hostname_mxit_urbandictionary = "us-cdbr-azure-west-b.cleardb.com";
 	$database_mxit_urbandictionary = "mxiturbandictionary";
 	$username_mxit_urbandictionary = "bcb8a82d9a9ece";
 	$password_mxit_urbandictionarys = "e3771f06";
-	*/
+	
 	$mxit_urbandictionary = mysql_pconnect($hostname_mxit_urbandictionary, $username_mxit_urbandictionary, $password_mxit_urbandictionarys) or trigger_error(mysql_error(),E_USER_ERROR); 
 	
 ?>
