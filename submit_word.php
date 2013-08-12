165732dab68365e67229cea53bbd0ff4405c850d
Tawanda Kembo <tkembo@gmail.com>
2013-08-12 06:53:20 +0200

    charge the user 20 moola to submit a word

--- submit_word.php
+++ submit_word.php
@@ -6,11 +6,18 @@
 </head>
 
 <body>
-<strong> I'm now charging 10 moola to submit a word </strong>
-<p>Because I have been receiving a high volume of SPAM, I am now charging 10 Moola to submit a word. I hope this will curb SPAM. Clicking the link or selecting the option below will charge you 10 Moola to submit a word</p>
+<strong> I'm now charging 20 moola to submit a word </strong>
+<p>Because I have been receiving a high volume of SPAM, I am now charging 20 Moola to submit a word. I hope this will curb SPAM. Clicking the link or selecting the option below will charge you 20 Moola to submit a word</p>
  <br />
-            <strong><a href="submit_word_start.php">1</a>) <a href="submit_word_start.php">Submit your own word</a>
-            </p> 
+<strong><a href="submit_word_start.php">1</a>) <a href="submit_word_start.php">Submit your own word</a>
+
+<strong>
+	
+	<a href="http://billing.internal.mxit.com/Transaction/PaymentRequest?VendorId=413&TransactionReference=<?php echo uniqid() ?>&CallbackUrl=http://<?php echo $_SERVER['SERVER_NAME']?>/submit_word_start.php&ProductId=PID001&ProductName=Submit+a+word+to+urbanditionary&ProductDescription=Submit+your+own+definition&MoolaAmount=20&CurrencyAmount=-">1</a>) 
+	
+	<a href="http://billing.internal.mxit.com/Transaction/PaymentRequest?VendorId=413&TransactionReference=<?php echo uniqid() ?>&CallbackUrl=http://<?php echo $_SERVER['SERVER_NAME']?>/submit_word_start.php&ProductId=PID001&ProductName=Submit+a+word+to+urbanditionary&ProductDescription=Submit+your+own+definition&MoolaAmount=20&CurrencyAmount=-">Submit your own word</a>
+	
+<strong></p> 
 
   	
 </body>
